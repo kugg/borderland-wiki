08731b448030a6c827f8f37197fbe8eea161a71f
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2013-08-22 12:07:30 +0200

    Revert "Making SSL connection true as default per disabling http for logged in users in 21 August"
    This reverts commit 6addd6f70a386fd131acbe5c2d0b47f21a0cd68f.
    
    The WMF is not switching for another week [1] and this change breaks editing
    on WMF sites due to a missing root certificate [2].
    
    [1] http://article.gmane.org/gmane.science.linguistics.wikipedia.technical/72200
    [2] https://sourceforge.net/p/pywikipediabot/bugs/1656/
    
    Change-Id: Ifab59ca6d9701b65ae779a28727b7b27c8193974

--- family.py
+++ family.py
@@ -4726,6 +4726,3 @@ class WikimediaFamily(Family):
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
-        
-    def protocol(self, code):
-        return 'https'
