131a5898cbbba0f76b44039ff7d98cd5f7370755
xqt <info@gno.de>
2013-09-08 12:32:15 +0200

    set valid protocol for 'client', remove 'repo' from family file, wikidata-test-repo.wikimedia.de is no longer availlable remove overriding defaults, they are no longer needed.
    Change-Id: I48ec1463d1b6bf87519e741f301b0d3a72aa9b51

--- families/wikidata_family.py
+++ families/wikidata_family.py
@@ -13,20 +13,11 @@ class Family(family.WikimediaFamily):
         self.name = 'wikidata'
         self.langs = {
             'wikidata': 'www.wikidata.org',
-            'repo': 'wikidata-test-repo.wikimedia.de',
             'client': 'wikidata-test-client.wikimedia.de',
             'test': 'test.wikidata.org',
         }
 
         # Override defaults
-        self.namespaces[0]['repo'] = [u'', u'Item']
-        self.namespaces[1]['repo'] = [u'Talk', u'Item talk']
-        del(self.namespaces[828]['_default'])
-        del(self.namespaces[829]['_default'])
-        self.namespaces[828]['test'] = u'Module'
-        self.namespaces[829]['test'] = u'Module talk'
-        self.namespaces[828]['wikidata'] = u'Module'
-        self.namespaces[829]['wikidata'] = u'Module talk'
 
         # Most namespaces are inherited from family.Family.
         # Translation used on all wikis for the different namespaces.
@@ -35,18 +26,10 @@ class Family(family.WikimediaFamily):
         self.namespaces[4] = {
             '_default': [u'Wikidata', u'WD', 'Project'],
             'client': u'Test Wikipedia',
-            'repo': u'Testwiki',
         }
         self.namespaces[5] = {
             '_default': [u'Wikidata talk', u'WT', 'Project talk'],
             'client': u'Test Wikipedia talk',
-            'repo': u'Testwiki talk',
-        }
-        self.namespaces[102] = {
-            'repo': u'Property',
-        }
-        self.namespaces[103] = {
-            'repo': u'Property talk',
         }
         self.namespaces[120] = {
             'test': u'Property',
@@ -65,12 +48,10 @@ class Family(family.WikimediaFamily):
             'wikidata': u'Query talk',
         }
         self.namespaces[1198] = {
-            'repo': u'Translations',
             'test': u'Translations',
             'wikidata': u'Translations',
         }
         self.namespaces[1199] = {
-            'repo': u'Translations talk',
             'test': u'Translations talk',
             'wikidata': u'Translations talk',
         }
@@ -93,4 +74,9 @@ class Family(family.WikimediaFamily):
             elif code == 'test':
                 return ('test', 'wikidata')
             else:
-                return ('repo', 'wikidata')
+                return (None, None)
+        
+    def protocol(self, code):
+        if code == 'client':
+            return 'http'
+        return super(Family, self).protocol(code)
