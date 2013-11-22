b5287526338f0db55dddf4ad7bf3b057929a83b5
Ladsgroup <ladsgroup@gmail.com>
2013-11-22 20:09:35 +0330

    Add 'wbremoveclaims' to postAC list
    Change-Id: I59ed40a2f110158d93af18cfaab6a7339ab42a7a

--- query.py
+++ query.py
@@ -100,7 +100,7 @@ def GetData(params, site=None, useAPI=True, retryCount=config.maxretries,
     postAC = [
         'edit', 'login', 'purge', 'rollback', 'delete', 'undelete', 'protect',
         'parse', 'block', 'unblock', 'move', 'emailuser', 'import',
-        'userrights', 'upload', 'patrol', 'wbcreateclaim', 'wbeditentity'
+        'userrights', 'upload', 'patrol', 'wbcreateclaim', 'wbeditentity', 'wbremoveclaims'
     ]
     if site.versionnumber() >= 18:
         postAC.append('watch')
