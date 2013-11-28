c7efc1b00a97da448df34865908e590b6a1adcc6
xqt <info@gno.de>
2013-11-28 18:01:20 +0100

    [PEP8] changes
    Change-Id: I45471e50e518fe4c954dbe189680792f3e02ab13

--- tag_nowcommons.py
+++ tag_nowcommons.py
@@ -5,14 +5,17 @@ Bot tag tag files available at Commons with the Nowcommons template.
 """
 #
 # (C) Multichill, 2011
-# (C) xqt, 2012
+# (C) xqt, 2013
 #
 # Distributed under the terms of the MIT license.
 #
 __version__ = '$Id$'
 #
 
-import sys, re, urllib
+import sys
+import re
+import urllib
+
 import wikipedia as pywikibot
 import pagegenerators
 import image
@@ -60,23 +63,25 @@ class NoEnoughData(pywikibot.Error):
 
 
 def tagNowCommons(page):
-    
+
     imagepage = pywikibot.ImagePage(page.site(), page.title())
     site = page.site()
     language = site.language()
     family = site.family.name
-    
+
     if not imagepage.fileIsOnCommons():
 
         if family in skips and language in skips[family]:
             localskips = skips[family][language]
         else:
             localskips = skips['_default']
-            
+
         for template in imagepage.templates():
             #FIXME: Move the templates list to a lib.
             if template in localskips:
-                pywikibot.output(u'The file %s is already tagged with NowCommons' % imagepage.title())
+                pywikibot.output(
+                    u'The file %s is already tagged with NowCommons'
+                    % imagepage.title())
                 return
 
         imagehash = imagepage.getHash()
@@ -87,7 +92,8 @@ def tagNowCommons(page):
             pywikibot.output(u'Found duplicate image at %s' % duplicate)
             comment = i18n.twtranslate(imagepage.site(),
                                        'commons-file-now-available',
-                                       {'localfile': imagepage.title(withNamespace=False),
+                                       {'localfile': imagepage.title(
+                                           withNamespace=False),
                                         'commonsfile': duplicate})
             template = pywikibot.translate(imagepage.site(), nowCommonsTemplate)
             newtext = imagepage.get() + template % (duplicate,)
@@ -97,8 +103,9 @@ def tagNowCommons(page):
             except wikipedia.LockedPage:
                 return
 
+
 def main(args):
-    generator = None;
+    generator = None
     always = False
 
     # Load a lot of default generators
@@ -107,16 +114,15 @@ def main(args):
     for arg in pywikibot.handleArgs():
         genFactory.handleArg(arg)
 
-
     generator = genFactory.getCombinedGenerator()
     if not generator:
-        raise NoEnoughData('You have to specify the generator you want to use for the script!')
+        raise NoEnoughData(
+            'You have to specify the generator you want to use for the script!')
 
     pregenerator = pagegenerators.PreloadingGenerator(generator)
-
     for page in pregenerator:
-        if page.exists() and (page.namespace() == 6) and \
-            (not page.isRedirectPage()):
+        if page.exists() and page.namespace() == 6 and \
+           not page.isRedirectPage():
             tagNowCommons(page)
 
 
