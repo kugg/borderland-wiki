9c04089c4ae34bc453fe539414474c46f85185d7
xqt <info@gno.de>
2013-11-30 11:16:03 +0100

    [PEP8] changes
    Change-Id: I11875fd91428b753bce05a1ed7f4605053677073

--- udp-log.py
+++ udp-log.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*-
 #
 # (C) Misza13 <misza1313@gmail.com>, 2007
-# (C) Pywikipedia bot team, 2007-2008, 2010
+# (C) Pywikibot team, 2007-20013
 #
 # Distributed under the terms of the MIT license.
 #
 __version__ = '$Id$'
 #
 
-import sys, re, socket
+import sys
+import re
+import socket
 
 TARGET_HOST = 'toolserver.org'
 TARGET_PORT = 42448
@@ -25,10 +27,10 @@ if log:
 
     ver = log.group('ver')
     user = log.group('user')
-    logmsg = log.group('logmsg').replace('\n',' ')
+    logmsg = log.group('logmsg').replace('\n', ' ')
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.connect((TARGET_HOST,TARGET_PORT))
+    sock.connect((TARGET_HOST, TARGET_PORT))
     sock.send('\002%s\002 commited \002%s\002 * \0032%s\003'
               % (user, ver, logmsg))
     sock.close()
