b888d41b339562b2f0928b990f95f00dc6ade94b
xqt <info@gno.de>
2013-12-22 08:31:55 +0100

    [PEP8] changes, do not override standard object identifier
    Change-Id: Ie975c1e7d3d18224eb42ad9a4796ab01e6cad8cc

--- saveHTML.py
+++ saveHTML.py
@@ -23,15 +23,22 @@ Features, not bugs:
 """
 
 # (C) 2004 Thomas R. Koll, <tomk32@tomk32.de>
-# (C) Pywikipedia bot team, 2004-2011
+# (C) Pywikibot team, 2004-2013
 #
 # Distributed under the terms of the MIT license.
 #
 __version__ = '$Id$'
 
-import httplib, StringIO, re, sys, md5, os, string
-import wikipedia as pywikibot
+import httplib
+import StringIO
+import re
+import sys
+import md5
+import os
+import string
+import pywikibot
 from htmlentitydefs import *
+
 
 def extractArticle(data):
     """ takes a string with the complete HTML-file
@@ -41,7 +48,8 @@ def extractArticle(data):
 
     images = []
     s = StringIO.StringIO(data)
-    rPagestats = re.compile('.*(\<span id\=(\"|\')pagestats(\"|\')\>.*\<\/span\>).*')
+    rPagestats = re.compile(
+        '.*(\<span id\=(\"|\')pagestats(\"|\')\>.*\<\/span\>).*')
     rBody = re.compile('.*<div id\=\"content\">.*')
     rFooter = re.compile('.*<div id\=\"footer\">.*')
     rDivOpen = re.compile('.*<div ')
@@ -49,9 +57,9 @@ def extractArticle(data):
     divLevel = 1
     divLast = -1
     inArticle = 0
-    inFooter  = 0
-    result = {'article':"",
-              'footer':""}
+    inFooter = 0
+    result = {'article': "",
+              'footer': ""}
     for line in s:
         if line == "<p><br /></p>":
             continue
@@ -65,7 +73,7 @@ def extractArticle(data):
             divLast = divLevel-2
         elif rFooter.match(line):
             divLast = divLevel-1
-            inFooter  = 1
+            inFooter = 1
         if inArticle:
             result['article'] += line
         elif inFooter:
@@ -76,27 +84,27 @@ def extractArticle(data):
                 inArticle = 0
                 inFooter = 0
                 divLast = -1
-
-
     return result
 
-def html2txt(str):
-    dict = {"%C3%A4": "ä",
-            "%C3%B6": "ö",
-            "%C3%BC": "ü",
-            "%C3%84": "Ä",
-            "%C3%96": "Ö",
-            "%C3%9C": "Ü",
-            "%C3%9F": "ß",
-            "%27": "'",
-            "%28": "(",
-            "%29": ")",
-            "%2C": ","
-            }
 
-    for entry in dict:
-        str = re.sub(entry, dict[entry], str)
-    return str
+def html2txt(s):
+    d = {"%C3%A4": "ä",
+         "%C3%B6": "ö",
+         "%C3%BC": "ü",
+         "%C3%84": "Ä",
+         "%C3%96": "Ö",
+         "%C3%9C": "Ü",
+         "%C3%9F": "ß",
+         "%27": "'",
+         "%28": "(",
+         "%29": ")",
+         "%2C": ","
+         }
+
+    for entry in d:
+        s = re.sub(entry, dict[entry], s)
+    return s
+
 
 def extractImages(data):
     """ takes a string with the complete HTML-file
@@ -105,8 +113,12 @@ def extractImages(data):
     contain information on last change """
 
     images = []
-    rImage = re.compile('<a href=[\r\n]*?"/wiki/.*?:(.*?)".*?[\r\n]*?.*?class=[\r\n]*?"image"', re.MULTILINE)
-    rThumb = re.compile('<a href=[\r\n]*?"/wiki/.*?:(.*?)".*?[\r\n]*?.*?class=[\r\n]*?"internal".*?[\r\n]*?.*?<img', re.MULTILINE or re.DOTALL)
+    rImage = re.compile(
+        '<a href=[\r\n]*?"/wiki/.*?:(.*?)".*?[\r\n]*?.*?class=[\r\n]*?"image"',
+        re.MULTILINE)
+    rThumb = re.compile(
+        '<a href=[\r\n]*?"/wiki/.*?:(.*?)".*?[\r\n]*?.*?class=[\r\n]*?"internal".*?[\r\n]*?.*?<img',
+        re.MULTILINE or re.DOTALL)
     last = ""
     img = rImage.findall(data)
     timg = rThumb.findall(data)
@@ -119,8 +131,8 @@ def extractImages(data):
 
     for image in img:
         path = md5.new(html2txt(image)).hexdigest()
-        images.append( {'image': image,
-                        'path' : str(path[0])+"/"+str(path[0:2])+"/"})
+        images.append({'image': image,
+                       'path': str(path[0]) + "/" + str(path[0:2]) + "/"})
     images.sort()
     return images
 
@@ -137,16 +149,16 @@ def main():
         if arg.startswith("-lang:"):
             lang = arg[6:]
         elif arg.startswith("-file:"):
-            f=open(arg[6:], 'r')
-            R=re.compile(r'.*\[\[([^\]]*)\]\].*')
+            f = open(arg[6:], 'r')
+            R = re.compile(r'.*\[\[([^\]]*)\]\].*')
             m = False
             for line in f.readlines():
-                m=R.match(line)
+                m = R.match(line)
                 if m:
                     sa.append(string.replace(m.group(1), " ", "_"))
                 else:
-                    print "ERROR: Did not understand %s line:\n%s" % (
-                        arg[6:], repr(line))
+                    print("ERROR: Did not understand %s line:\n%s"
+                          % arg[6:], repr(line))
             f.close()
         elif arg.startswith("-o:"):
             output_directory = arg[3:]
@@ -173,13 +185,14 @@ def main():
     data = ""
     for article in sa:
         filename = article.replace("/", "_")
-        if os.path.isfile(output_directory + filename + ".txt") and overwrite_articles == False:
+        if not overwrite_articles and os.path.isfile(
+                output_directory + filename + ".txt"):
             print "skipping " + article
             continue
         data = ""
         ua = article
         while len(data) < 2:
-            url = '/wiki/'+ ua
+            url = '/wiki/' + ua
             conn.request("GET", url, "", headers)
             response = conn.getresponse()
             data = response.read()
@@ -190,35 +203,36 @@ def main():
                 print ua
 
         data = extractArticle(data)
-        f = open (output_directory + filename + ".txt", 'w')
-        f.write (data['article'] + '\n' + data['footer'])
+        f = open(output_directory + filename + ".txt", 'w')
+        f.write(data['article'] + '\n' + data['footer'])
         f.close()
         print "saved " + article
 
         if save_images:
             images = extractImages(data['article'])
             for i in images:
-                if overwrite_images == False and os.path.isfile(output_directory + i['image']):
+                if not overwrite_images and os.path.isfile(
+                        output_directory + i['image']):
                     print "skipping existing " + i['image']
                     continue
                 print 'downloading ' + i['image'],
                 uo = pywikibot.MyURLopener
-                file = uo.open( "http://upload.wikimedia.org/wikipedia/"
-                                +mysite.lang + '/' + i['path'] + i['image'])
+                file = uo.open("http://upload.wikimedia.org/wikipedia/"
+                               + mysite.lang + '/' + i['path'] + i['image'])
                 content = file.read()
-                if (len(content) < 500):
+                if len(content) < 500:
                     uo.close()
                     print "downloading from commons",
                     uo = pywikibot.MyURLopener
-                    file = uo.open( "http://commons.wikimedia.org/upload/"
-                                    + i['path'] + i['image'])
-                    #print "http://commons.wikimedia.org/upload/", i['path'] , i['image'], file
+                    file = uo.open("http://commons.wikimedia.org/upload/"
+                                   + i['path'] + i['image'])
                     content = file.read()
                 f = open(output_directory + i['image'], "wb")
                 f.write(content)
                 f.close()
                 print "\t\t", (len(content)/1024), "KB done"
     conn.close()
+
 
 if __name__ == "__main__":
     main()
