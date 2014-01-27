799666eff164a7397e740cd7b6957e6f1de1b44d
xqt <info@gno.de>
2014-01-27 07:58:41 +0100

    update language_by_size
    Change-Id: I1a512043361952e07da157246c1be759515acbd7

--- families/wiktionary_family.py
+++ families/wiktionary_family.py
@@ -13,17 +13,17 @@ class Family(family.WikimediaFamily):
         self.languages_by_size = [
             'en', 'mg', 'fr', 'zh', 'lt', 'ru', 'es', 'el', 'nl', 'pl', 'sv',
             'ko', 'de', 'tr', 'ku', 'ta', 'io', 'kn', 'fi', 'vi', 'hu', 'pt',
-            'chr', 'no', 'ml', 'my', 'id', 'it', 'li', 'ro', 'et', 'ja', 'te',
+            'chr', 'no', 'ml', 'my', 'id', 'it', 'li', 'ro', 'ja', 'et', 'te',
             'jv', 'fa', 'cs', 'ca', 'ar', 'eu', 'gl', 'lo', 'uk', 'br', 'fj',
             'eo', 'bg', 'hr', 'th', 'oc', 'is', 'vo', 'ps', 'zh-min-nan',
             'simple', 'cy', 'uz', 'scn', 'sr', 'af', 'ast', 'az', 'da', 'sw',
-            'fy', 'tl', 'he', 'nn', 'wa', 'ur', 'la', 'sq', 'hy', 'sm', 'sl',
+            'fy', 'he', 'tl', 'nn', 'wa', 'ur', 'la', 'hy', 'sq', 'sm', 'sl',
             'ka', 'pnb', 'nah', 'hi', 'tt', 'bs', 'lb', 'lv', 'tk', 'sk', 'hsb',
             'nds', 'kk', 'ky', 'be', 'km', 'mk', 'ga', 'wo', 'ms', 'ang', 'co',
             'sa', 'gn', 'mr', 'csb', 'ug', 'st', 'ia', 'sd', 'sh', 'si', 'mn',
             'tg', 'or', 'kl', 'vec', 'jbo', 'an', 'ln', 'fo', 'zu', 'gu', 'kw',
             'gv', 'rw', 'qu', 'ss', 'ie', 'mt', 'om', 'bn', 'pa', 'roa-rup',
-            'iu', 'so', 'am', 'su', 'za', 'gd', 'mi', 'tpi', 'ne', 'yi', 'na',
+            'iu', 'so', 'su', 'am', 'za', 'gd', 'mi', 'tpi', 'ne', 'yi', 'na',
             'ti', 'sg', 'dv', 'tn', 'ts', 'ha', 'ks', 'ay',
         ]
 
