471b0c15be5dc63ff1ab075371b3feae6ad4fd88
xqt <info@gno.de>
2014-01-31 14:36:33 +0100

    new mw release 1.23wmf11
    Change-Id: Icf669bd233eac16bb906e58703c867a8458291ab

--- family.py
+++ family.py
@@ -4942,7 +4942,7 @@ class WikimediaFamily(Family):
         """Return Wikimedia projects version number as a string."""
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
-        return '1.23wmf10'
+        return '1.23wmf11'
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
