bb3490260471d4d8d90c7495b58b353cbb8f7609
xqt <info@gno.de>
2015-02-26 10:54:30 +0100

    update wm version
    Change-Id: Icd729ef16454f76f93e35afef31c9f7ed62425b9

--- family.py
+++ family.py
@@ -4967,7 +4967,7 @@ class WikimediaFamily(Family):
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
         # Here we return the latest mw release of wikimedia projects
-        return '1.25wmf16'
+        return '1.25wmf18'
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
